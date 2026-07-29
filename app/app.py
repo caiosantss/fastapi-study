d86e083c4b67d471ad19cd15ef13f452721495a7
Caio Santos <caiosilvasantos87@gmail.com>
2026-07-28 21:57:51 -0300

    feat: Contrato pydantic e test de endpoint com html

--- app/app.py
+++ app/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from app.schemas import Mensagem
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Mensagem)
 def read_root():
-    return {'message': 'Hello World'}
+    return Mensagem(mensagem='Hello World')
+
+
+@app.get('/page', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def render_hello_word():
+    return """
+        <html>
+        <head>
+            <title>Sem titulo</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+            <p>PA!</p>
+        </body>
+        </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,4 +12,16 @@ def test_read_root_retorna_ok_e_ola_mundo():
     response = client.get('/')
     # Assert
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello World'}
+    assert response.json() == {'mensagem': 'Hello World'}
+
+
+def test_page_retorna_html():
+    # Arrange
+    client = TestClient(app=app)
+
+    # Act
+    response = client.get('/page')
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert '<p>PA!</p>' in response.text
