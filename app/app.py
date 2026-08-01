1e8a4015802e9b9b876660a0fde0e3977c8ec23e
Caio Santos <caiosilvasantos87@gmail.com>
2026-08-01 16:22:33 -0300

    feat: POST e schemas

--- app/app.py
+++ app/app.py
@@ -3,9 +3,10 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from app.schemas import Mensagem
+from app.schemas import Mensagem, UserDB, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='FastAPI TOP!')
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Mensagem)
@@ -13,7 +14,9 @@ def read_root():
     return Mensagem(mensagem='Hello World')
 
 
-@app.get('/page', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+@app.get(
+    '/hello-world/', status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
 def render_hello_word():
     return """
         <html>
@@ -25,3 +28,10 @@ def render_hello_word():
             <p>PA!</p>
         </body>
         </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Mensagem(BaseModel):
     mensagem: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+    id: int
